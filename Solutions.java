46791303caba19df8fac36d4f023c44cf9ea54e1
doshiakshata <114414315+doshiakshata@users.noreply.github.com>
2023-01-16 16:28:34 +0530

    Add files via upload

---
+++ Solutions.java
@@ -0,0 +1,41 @@
+package solution.com;
+import java.util.Arrays;
+
+public class Solutions {
+	public static void main(String[]args)
+	{
+	class Job{
+        int start, end, profit;
+        Job(int start, int end, int profit){
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+        }
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = profit.length;
+        Job []jobs = new Job[n];
+        for(int i=0; i<n; i++){
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        }
+        
+        Arrays.sort(jobs, (a,b) -> (a.end-b.end));              // sorting based on end time
+        
+        int dp[] = new int[n];                     // dp[i] = maximum profit earned till jobs(1...i)
+        dp[0] = jobs[0].profit;
+        
+        for(int i = 1; i < n; i++){
+            
+            dp[i] = Math.max(dp[i-1], jobs[i].profit);
+            for(int j = i-1; j >= 0; j--){
+                if(jobs[j].end <= jobs[i].start){
+                    dp[i] = Math.max(dp[i], jobs[i].profit + dp[j]);
+                    break;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+	}
+	}
+}
+	
